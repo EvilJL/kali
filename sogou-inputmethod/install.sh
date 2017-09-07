98264d7baa4b6fb41e9c568cffc39e9be734d5f6
JINLINBI <898141731@qq.com>
2017-09-08 01:31:28 +0800

    add fcitx pkg

--- sogou-inputmethod/install.sh
+++ sogou-inputmethod/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkg_name="sogoupinyin.deb"
 pkg_url="http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=nvYWDuFAIspH35BPsQh8EA&e=1504321129&fn=sogoupinyin_2.1.0.0086_amd64.deb"
-pkg_dep="libopencc2 libopencc2-data"
+pkg_dep="libopencc2 libopencc2-data fcitx fcitx-pinyin"
 is_installed=`apt list sogoupinyin|grep installed`
 if [ "$is_installed" = "" ]; then
         echo "即将安装搜狗输入法："
