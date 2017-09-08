a15c455ebcfdce920b6ca02c0dc99a94d61688c2
张进奕 <zjy@pv.cc>
2017-09-08 15:16:08 +0800

    修改sshclient脚本错误

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cat >> /etc/ssh/ssh_config <<EOF
-    ServerAliveInterval 20      # 心跳包间隔
-    ServerAliveCountMax 999     # 最大失联次数
+    ServerAliveInterval 20
+    # 心跳包间隔
+    ServerAliveCountMax 999
+    # 最大失联次数
 EOF
