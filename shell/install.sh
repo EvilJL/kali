17138e9f3427ba0d41499e00bd17bf82c8d0f74c
张进奕 <zjy@pv.cc>
2017-09-08 08:32:41 +0800

    添加login部署shell文件

---
+++ shell/install.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: shell/install.sh
+# Author: jinlin
+# mail:898141731@qq .com
+# Created Time: Friday, September 08, 2017 AM08:30:42 HKT
+#########################################################################
+#!/bin/bash
+chmod +x *
+cp * /usr/local/bin/*
